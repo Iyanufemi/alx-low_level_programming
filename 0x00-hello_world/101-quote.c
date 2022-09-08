98b8e2063cf19884719824cc9097d072bd9e7558
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-08 07:05:28 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * main - this program prints exactly "and that piece of are is useful" - Dora Korpar, 2015-10-19"
  *
@@ -6,9 +7,7 @@
  */
 int main(void)
 {
-	write(2
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	write(2	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 
 	return (1);
 }
