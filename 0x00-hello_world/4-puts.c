dc1d99b12251613648397e0a916494c4387127b2
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-08 05:31:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+/*
+ * This is task 4
+ * The program prints exactly 'programming is like building a multilingual puzzle'
+ * it then returns 0
+ */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
