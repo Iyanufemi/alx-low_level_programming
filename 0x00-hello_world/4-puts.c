0a19043e93f7c62a071ca3faaab4343b1d9e703c
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-08 05:37:47 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
+#include <stdio.h>
 /*
  * This is task 4
  * The program prints exactly 'programming is like building a multilingual puzzle'
  * it then returns 0
  */
-
-#include <stdio.h>
-
-
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
