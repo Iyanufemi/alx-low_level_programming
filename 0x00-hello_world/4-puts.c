9f0b659ac257b46c75429239e518d0413ba1db94
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-08 05:49:31 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
- * This is task 4
- * The program prints exactly 'programming is like building a multilingual puzzle'
- * it then returns 0
+
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 }
