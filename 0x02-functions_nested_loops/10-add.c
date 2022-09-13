7830f1277c2e5a219be19fd301b81fcee75cf2ab
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-13 11:16:28 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @no1: first integer
+ * @no2: second interger
+ *
+ * Return: The result of the addition.
+ */
+int add(int no1, int no2)
+{
+	return (no1 + no2)
+}
