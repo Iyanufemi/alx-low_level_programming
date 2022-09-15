16a1d93408cd05870e9996a2da9d411f9d8e64fa
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-15 12:47:42 -0700

    done

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Description: Header file containing prototypes for all functions
- * used in the 0x04-more_functions_nested_loops directory.
- */
 int _putchar(char a);
 void print_alphabet(void);
 void print_alphabet_x10(void);
