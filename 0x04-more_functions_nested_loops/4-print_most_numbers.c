ad2a45a37cccf5d147d8c152847fd914a776d600
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-15 06:16:11 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from 0 to 9, but 2 and 4
- *
- * Return: void
+ * print_most_numbers - prints the numbers from
+ * 0-9 except for 2 and 4.
  */
+
 void print_most_numbers(void)
 {
 	int num;
