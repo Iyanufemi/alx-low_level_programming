d1d3dd0617c746e610f5dcb63bc62dbd11274c11
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-15 03:26:52 -0700

    LARGEST

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * largest_number - prints the largest of 3 numbers
+ * @x: first integer
+ * @y: second integer
+ * @z: third integer
+ *
+ * Return: largest number
+ */
+
+int largest_number(int x, int y, int z)
+{
+	int largest;
+
+	if (x >= y && x >= z)
+	{
+		largest = x;
+	}
+	else if (y >= x && y >= z)
+	{
+		largest = y;
+	}
+	else if (z >= x && z >= y)
+	{
+		largest = z;
+	}
+
+	return (largest);
+}
