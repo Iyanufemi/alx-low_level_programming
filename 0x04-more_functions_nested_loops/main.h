146cb3af01861b29012812a0d92b3da8378186e3
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-15 06:56:15 -0700

    fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,8 +6,23 @@
  * Description: Header file containing prototypes for all functions
  * used in the 0x04-more_functions_nested_loops directory.
  */
-int _isupper(int c);
+int _putchar(char a);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
 int _isdigit(int c);
+int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
@@ -16,6 +31,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-int _putchar(char c);
-void print_number(int n);
 #endif
