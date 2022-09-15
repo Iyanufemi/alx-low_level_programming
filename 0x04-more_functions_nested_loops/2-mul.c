53f96928ba939d0984abf0c92fbb5472d8fdbbcb
Iyanu Femi Akinladenu <iyanu.akinladenu@gmail.com>
2022-09-15 00:44:44 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -  multiplies two integers.
+ * @x: The number to be checked
+ * @y: The number to be checked
+ *
+ * Return: 0
+ */
+int mul(int x, int y)
+{
+	return (a * b);
+}
