c9cc3e2c2f51c4cfde0eccf35d0599bcc255027e
iyanufemi <iyanu.akinladenu@gmail.com>
2022-09-26 11:39:46 -0700

    third commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,19 +7,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	unsigned int i, j, acc;
+
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		bool = 1;
+		acc = 1;
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
-				bool = 0;
+				acc = 0;
 				break;
 			}
 		}
-		if (bool == 1)
+		if (acc == 1)
 			break;
 	}
 	return (i);
