af5a843047a5ab9789227be2a4eb384afaaf8924
iyanufemi <iyanu.akinladenu@gmail.com>
2022-09-26 12:02:53 -0700

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,16 +6,12 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, m = 0;
+	unsigned int j, b;
 
-	for (i = 0; i < 64; i++)
+	for (j = 0; j < 8; j++)
 	{
-		if (i % 8 == 0 && i != 0)
-		{
-			m = i;
-			_putchar('\n');
-		}
-		_putchar(a[i / 8][i - m]);
+		for(b = 0; b < 8; b++)
+			_putchar(a[j][b]);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
