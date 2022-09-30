214371a9a4924889111b452676d7562b373315bb
iyanufemi <iyanu.akinladenu@gmail.com>
2022-09-30 14:11:16 -0700

    fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,11 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	if (argv[0])
+	{
 		printf("%d\n", argc - 1);
+	}
 	return(0);
 }
