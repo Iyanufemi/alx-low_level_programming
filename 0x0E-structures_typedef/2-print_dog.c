6b3eae1a677c687b04bde017ba91078155d18df5
iyanufemi <iyanu.akinladenu@gmail.com>
2022-10-10 12:42:19 -0700

    second commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,25 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_dog - prints a struct dog.
- * @d: struct dog.
- *
- * Return: no return.
+ * print_dog - a function that prints a struct dog
+ * @d: struct to print
+ * Return: Nothing.
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d == NULL)
 	{
-		if (!(d->name))
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
-
-		printf("Age: %f\n", d->age);
-
-		if (!(d->owner))
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
+		return;
 	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+
+	printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,12 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-/**
- * struct dog - struct that stores some information of a dog
- * @name: name of the dog
- * @age: age of the dog
+ * struct dog - structure of a dog
+ * @name: dogs name
+ * @age: dogs age
  * @owner: owner of the dog
- *
- * Description: struct called "dog" that stores its name, its age
- * and the name of its owner.
+ * Description: data structure for a dog
  */
 struct dog
 {
@@ -21,10 +14,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for strut dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
